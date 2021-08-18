7938cb0b5d955884fe06c401eb782f9428307e73
Alexis Guzman <alexis12989x@gmail.com>
2021-08-18 09:52:00 -0500

    feat: set up asterisks component

---
+++ components/home/Asterisks.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+import asteriskOne from "@/public/asterisk-one.svg";
+import asteriskTwo from "@/public/asterisk-two.svg";
+
+const Asterisks = () => {
+  return (
+    <div className="mx-auto relative top-0 h-full w-full max-w-screen-xl">
+      <div className="absolute -top-20 left-20">
+        <Image alt="first asterisk" src={asteriskOne} />
+      </div>
+      <div className="absolute bottom-8 right-20">
+        <Image alt="second asterisk" src={asteriskTwo} />
+      </div>
+    </div>
+  );
+};
+
+export default Asterisks;
