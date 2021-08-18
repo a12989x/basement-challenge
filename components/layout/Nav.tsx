9a39d98a335fcc31c1d407da4527aecc8b2e8c3a
Alexis Guzman <alexis12989x@gmail.com>
2021-08-18 09:51:22 -0500

    feat: add uppercase class name

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -5,11 +5,13 @@ import hd from "@/public/hd-4k.svg";
 
 const Nav = (): JSX.Element => {
   return (
-    <nav className="flex justify-between">
+    <nav className="pt-8 mx-auto max-w-screen-xl flex justify-between">
       <Image alt="Basement" src={logo} />
       <Image alt="hd-4k icons" src={hd} />
 
-      <button className="py-3.5 px-8 font-bold border border-white rounded-full">Checkout</button>
+      <button className="py-3.5 px-8 font-bold uppercase border border-white rounded-full">
+        Cart
+      </button>
     </nav>
   );
 };
