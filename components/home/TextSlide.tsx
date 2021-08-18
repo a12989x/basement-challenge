e2c90d9b20a03d66e752f47b78088f83c1c68dd0
Alexis Guzman <alexis12989x@gmail.com>
2021-08-18 09:53:49 -0500

    feat: set up text slide component

---
+++ components/home/TextSlide.tsx
@@ -0,0 +1,17 @@
+const TextSlide = (): JSX.Element => {
+  return (
+    <div
+      className="overflow-x-hidden w-full relative border-t border-b"
+      style={{height: "75px", transform: "translate3d(0, 0, 0)"}}
+    >
+      <p
+        className="my-4 absolute left-0 top-0 font-bold text-4xl animate-slideshow"
+        style={{width: "300%", transform: "translate3d(0, 0, 0)"}}
+      >
+        A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag
+      </p>
+    </div>
+  );
+};
+
+export default TextSlide;
