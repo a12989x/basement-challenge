ea799e0338fc472538ba2a04c95aca621ff6bde2
Alexis Guzman <alexis12989x@gmail.com>
2021-08-18 09:50:57 -0500

    feat: wrap image  with div and class names

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 import footer from "@/public/footer.svg";
 
 const Footer = (): JSX.Element => {
-  return <Image alt="footer" src={footer} />;
+  return (
+    <div className="px-8 mt-20 mx-auto max-w-screen-xl">
+      <Image alt="footer" src={footer} />
+    </div>
+  );
 };
 
 export default Footer;
