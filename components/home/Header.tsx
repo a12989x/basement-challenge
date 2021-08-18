3746c768fbfbd79a8567fb2ec0b25b665dd49bbd
Alexis Guzman <alexis12989x@gmail.com>
2021-08-18 09:53:18 -0500

    feat: set up header component

---
+++ components/home/Header.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import header from "@/public/header.svg";
+
+const Header = (): JSX.Element => {
+  return (
+    <header className="my-12 mx-auto max-w-screen-xl flex justify-center">
+      <Image alt="Basement" src={header} />
+    </header>
+  );
+};
+
+export default Header;
