083a8d69d149f298f6c746e92c1a2abbc571bcaa
Alexis Guzman <alexis12989x@gmail.com>
2021-08-19 17:38:40 -0500

    feat: set up product item component

---
+++ components/layout/product-item/index.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+import Quantity from "./Quantity";
+import Label from "./Label";
+
+import {Product} from "@/product/types";
+
+const ProductItem = ({product}: {product: Product}): JSX.Element => {
+  return (
+    <div className="p-4 grid gap-5 border-2 uppercase" style={{gridTemplateColumns: "auto 1fr"}}>
+      <div
+        className="px-10"
+        style={{
+          background: "linear-gradient(0deg, #1D1D1D 0%, rgba(21, 21, 21, 0) 100%)",
+        }}
+      >
+        <Image alt={product.name} height="269" src={product.image} width="200" />
+      </div>
+
+      <div className="flex flex-col justify-between">
+        <div>
+          <p className="text-4xl">{product.name}</p>
+          <p className="text-2xl capitalize text-gray-500">{product.description}</p>
+        </div>
+
+        <div className="grid gap-2">
+          <Quantity qty={product.qty} />
+
+          <div className="flex justify-between">
+            {product.options.map((option) => (
+              <Label key={option.label} option={option} />
+            ))}
+
+            <p className="text-3xl">${product.price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
