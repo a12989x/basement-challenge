d21e2cb70aadb79b957b44727ee43d52dc10e23e
Alexis Guzman <alexis12989x@gmail.com>
2021-08-19 17:39:33 -0500

    feat(styles): add padding responsive classnames

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import footer from "@/public/footer.svg";
 
 const Footer = (): JSX.Element => {
   return (
-    <div className="px-8 mt-20 mx-auto max-w-screen-xl">
+    <div className="px-4 sm:px-8 mt-20 mx-auto max-w-screen-xl">
       <Image alt="footer" src={footer} />
     </div>
   );
