4f8c0eb25c64b71e3501cdd655fbce60bf8f0f88
Alexis Guzman <alexis12989x@gmail.com>
2021-08-19 17:19:26 -0500

    feat(styles): add responsive class names

--- components/home/products/index.tsx
+++ components/home/products/index.tsx
@@ -4,7 +4,7 @@ import {Product} from "product/types";
 
 const Products = ({products}: {products: Product[]}): JSX.Element => {
   return (
-    <section className="mt-24 mx-auto max-w-screen-xl grid grid-cols-3 gap-8">
+    <section className="px-4 mt-24 mx-auto max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
       {products.map((productItem) => (
         <ProductItem key={productItem.id} productItem={productItem} />
       ))}
