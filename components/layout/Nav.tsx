c6f216317cc6082462b0f92e4a21bc14b0010177
Alexis Guzman <alexis12989x@gmail.com>
2021-08-19 17:39:56 -0500

    feat(context): import cart context

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,17 +1,38 @@
 import Image from "next/image";
+import {useContext, useState} from "react";
+
+import Modal from "./Modal";
 
 import logo from "@/public/logo.svg";
+import favicon from "@/public/favicon.svg";
 import hd from "@/public/hd-4k.svg";
+import {CartContext} from "contexts/CartProvider";
 
 const Nav = (): JSX.Element => {
-  return (
-    <nav className="pt-8 mx-auto max-w-screen-xl flex justify-between">
-      <Image alt="Basement" src={logo} />
-      <Image alt="hd-4k icons" src={hd} />
+  const {state} = useContext(CartContext);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-      <button className="py-3.5 px-8 font-bold uppercase border border-white rounded-full">
-        Cart
+  return (
+    <nav className="pt-8 px-4 sm:px-0 mx-auto max-w-screen-xl flex justify-between">
+      <div className="hidden sm:block">
+        <Image alt="Basement" src={logo} />
+      </div>
+      <div className="block sm:hidden">
+        <Image alt="Basement" height="45" src={favicon} width="54" />
+      </div>
+
+      <div className="hidden sm:inline-block">
+        <Image alt="hd-4k icons" src={hd} />
+      </div>
+
+      <button
+        className="py-2 px-5 sm:py-3.5 sm:px-8 font-bold uppercase border border-white rounded-full"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Cart ({state.length})
       </button>
+
+      {isModalOpen && <Modal closeModal={() => setIsModalOpen(false)} products={state} />}
     </nav>
   );
 };
