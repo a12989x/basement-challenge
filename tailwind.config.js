ae683c6fa6fe9e6d86b71a844896d8f29fc01d1c
Alexis Guzman <alexis12989x@gmail.com>
2021-08-19 17:42:57 -0500

    feat(styles): create slideshow animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideshow: {
+          "100%": {transform: "translateX(-200%)"},
+        },
+        animations: {
+          slideshow: "slideshow 12s linear infinite",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
