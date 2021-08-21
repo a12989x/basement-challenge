d6151546e49bbce7c4cc81ac09d4d46ff00cff8e
Alexis Guzman <alexis12989x@gmail.com>
2021-08-21 17:16:28 -0500

    feat: add dispatch funtions for plus and minus one

--- components/layout/product-item/Quantity.tsx
+++ components/layout/product-item/Quantity.tsx
@@ -1,12 +1,23 @@
-const Quantity = ({qty}: {qty: number | undefined}) => {
+import {useContext} from "react";
+
+import {CartContext} from "@/contexts/CartProvider";
+import {MINUS_ONE, PLUS_ONE} from "@/store/actions/cartActions";
+
+const Quantity = ({qty, id}: {qty: number | undefined; id: string}) => {
+  const {dispatch} = useContext(CartContext);
+
   return (
     <div className="flex items-center text-xl">
       <p>Quantity: </p>
 
       <div className="px-3 ml-4 grid grid-cols-3 gap-2 border rounded-full">
-        <button className="text-2xl">-</button>
+        <button className="text-2xl" onClick={() => dispatch({type: MINUS_ONE, payload: id})}>
+          -
+        </button>
         <span>{qty}</span>
-        <button className="text-2xl">+</button>
+        <button className="text-2xl" onClick={() => dispatch({type: PLUS_ONE, payload: id})}>
+          +
+        </button>
       </div>
     </div>
   );
