0534cc7ce9f0c7decb362b3d59fb673901a1f017
Alexis Guzman <alexis12989x@gmail.com>
2021-08-21 17:15:38 -0500

    feat(styles): update class names

--- components/home/TextSlide.tsx
+++ components/home/TextSlide.tsx
@@ -2,13 +2,19 @@ const TextSlide = (): JSX.Element => {
   return (
     <div
       className="overflow-x-hidden w-full relative border-t border-b"
-      style={{height: "75px", transform: "translate3d(0, 0, 0)"}}
+      // style={{height: "75px", transform: "translate3d(0, 0, 0)"}}
     >
       <p
-        className="my-4 absolute left-0 top-0 font-bold text-4xl animate-slideshow"
-        style={{width: "400%", transform: "translate3d(0, 0, 0)"}}
+        className="my-4 font-bold text-4xl animate-slideshow"
+        style={{width: "105%", transform: "translateX(100%)"}}
       >
-        A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag
+        A man can’t have enough base­ment swag
+      </p>
+      <p
+        className="my-4 font-bold text-4xl animate-slideshow"
+        style={{width: "105%", transform: "translateX(100%)"}}
+      >
+        A man can’t have enough base­ment swag
       </p>
     </div>
   );
