68d06dc4e146537f9fc34a011066279f2d3602ab
Alexis Guzman <alexis12989x@gmail.com>
2021-08-21 17:20:35 -0500

    refact(reducer): create local variables for each case

--- store/reducers/cartReducer.tsx
+++ store/reducers/cartReducer.tsx
@@ -1,30 +1,87 @@
-import {Product} from "product/types";
-import {ADD_PRODUCT, DELETE_PRODUCT, PLUS_ONE} from "store/actions/cartActions";
+import {Product, IOptionCart} from "product/types";
+import {
+  ADD_PRODUCT,
+  CHANGE_LABEL,
+  DELETE_PRODUCT,
+  MINUS_ONE,
+  PLUS_ONE,
+} from "store/actions/cartActions";
 
 export type CartAction =
   | {type: "ADD_PRODUCT"; payload: Product}
   | {type: "DELETE_PRODUCT"}
-  | {type: "PLUS_ONE"; payload: Product};
+  | {type: "PLUS_ONE"; payload: string}
+  | {type: "MINUS_ONE"; payload: string}
+  | {type: "CHANGE_LABEL"; payload: {id: string; cart: IOptionCart}};
 
 export const initialState: Product[] = [];
 
 const cartReducer = (store: Product[], action: CartAction) => {
   switch (action.type) {
-    case ADD_PRODUCT:
-      if (store.find((product) => product.id === action.payload.id)) {
-        const productIndexOf = store.indexOf(
-          store.find((product) => product.id === action.payload.id)!,
-        );
+    case ADD_PRODUCT: {
+      const products = store;
+      const newProduct = action.payload;
+      const isProductInProducts = products.find((product) => product.id === newProduct.id);
 
-        store[productIndexOf].qty! += 1;
-      } else store.push({...action.payload, qty: 1});
+      if (isProductInProducts) {
+        const productIndexOf = products.indexOf(isProductInProducts);
 
-      return [...store];
+        products[productIndexOf].qty! += 1;
+      } else
+        products.push({
+          ...newProduct,
+          qty: 1,
+          cart: action.payload.options.map((option) => ({
+            label: option.label,
+            value: option.values[0],
+          })),
+        });
 
-    case PLUS_ONE:
-      store[store.indexOf(store.find((product) => product.id === action.payload.id)!)].qty! += 1;
+      return [...products];
+    }
 
-      return [...store];
+    case PLUS_ONE: {
+      const products = store;
+      const productId = action.payload;
+      const productToUpdate = products.find((product) => product.id === productId);
+      const productToUpdateIndexOf = products.indexOf(productToUpdate!);
+
+      products[productToUpdateIndexOf].qty!++;
+
+      return [...products];
+    }
+
+    case MINUS_ONE: {
+      const products = store;
+      const productId = action.payload;
+      const productToUpdate = products.find((product) => product.id === productId);
+      const productToUpdateIndexOf = products.indexOf(productToUpdate!);
+
+      if (products[productToUpdateIndexOf].qty! > 0) {
+        products[productToUpdateIndexOf].qty!--;
+      } else return [...products];
+
+      return [...products];
+    }
+
+    case CHANGE_LABEL: {
+      const products = store;
+      const productId = action.payload.id;
+      const productLabel = action.payload.cart.label;
+      const productValue = action.payload.cart.value;
+      const productToUpdate = products.find((product) => product.id === productId);
+      const productToUpdateIndexOf = products.indexOf(productToUpdate!);
+      const productToUpdateCartLabel = products[productToUpdateIndexOf].cart!.find(
+        (cartOption) => cartOption.label === productLabel,
+      );
+      const productToUpdateCartLabelIndexOf = products[productToUpdateIndexOf].cart!.indexOf(
+        productToUpdateCartLabel!,
+      );
+
+      products[productToUpdateIndexOf].cart![productToUpdateCartLabelIndexOf].value = productValue;
+
+      return [...products];
+    }
 
     case DELETE_PRODUCT:
       return [...store];
