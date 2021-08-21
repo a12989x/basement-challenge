34b115bc93ea846394bce61fd4c9bfba9879de30
Alexis Guzman <alexis12989x@gmail.com>
2021-08-21 17:17:02 -0500

    feat(context): add dispatch function for change label

--- components/layout/product-item/Label.tsx
+++ components/layout/product-item/Label.tsx
@@ -1,13 +1,33 @@
-import {Option} from "@/product/types";
+import {useContext} from "react";
 
-const Label = ({option}: {option: Option}): JSX.Element => {
+import {CartContext} from "@/contexts/CartProvider";
+import {Option} from "@/product/types";
+import {CHANGE_LABEL} from "@/store/actions/cartActions";
+
+const Label = ({
+  option,
+  id,
+  currentValue,
+}: {
+  option: Option;
+  id: string;
+  currentValue: string;
+}): JSX.Element => {
+  const {dispatch} = useContext(CartContext);
+
   return (
     <div className="flex items-center text-xl">
       <p>{option.label}</p>
 
       <div className="ml-4 grid gap-2 grid-flow-col">
         {option.values.map((value) => (
-          <button key={value} className="px-2 border rounded-full">
+          <button
+            key={value}
+            className={`px-2 ${value === currentValue ? "border rounded-full" : ""}`}
+            onClick={() =>
+              dispatch({type: CHANGE_LABEL, payload: {id, cart: {label: option.label, value}}})
+            }
+          >
             {value}
           </button>
         ))}
