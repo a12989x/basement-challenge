d68914e4ff0db1de0a5d3ff3bb5e5acd4198bcde
Alexis Guzman <alexis12989x@gmail.com>
2021-08-22 17:42:44 -0500

    feat(styles): change display to flex

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import {useContext, useState} from "react";
+import Image from "next/image";
 
 import Modal from "./Modal";
 
-import logo from "@/public/logo.svg";
+import {CartContext} from "@/contexts/CartProvider";
 import favicon from "@/public/favicon.svg";
 import hd from "@/public/hd-4k.svg";
-import {CartContext} from "contexts/CartProvider";
+import logo from "@/public/logo.svg";
 
 const Nav = (): JSX.Element => {
   const {state} = useContext(CartContext);
@@ -14,19 +14,19 @@ const Nav = (): JSX.Element => {
 
   return (
     <nav className="pt-8 px-4 sm:px-0 mx-auto max-w-screen-xl flex justify-between">
-      <div className="hidden sm:block">
+      <div className="hidden sm:flex">
         <Image alt="Basement" src={logo} />
       </div>
-      <div className="block sm:hidden">
+      <div className="flex sm:hidden">
         <Image alt="Basement" height="45" src={favicon} width="54" />
       </div>
 
-      <div className="hidden sm:inline-block">
+      <div className="hidden sm:flex">
         <Image alt="hd-4k icons" src={hd} />
       </div>
 
       <button
-        className="py-2 px-5 sm:py-3.5 sm:px-8 font-bold uppercase border border-white rounded-full"
+        className="py-2 px-5 sm:py-3.5 sm:px-8 font-bold uppercase border border-white rounded-full transition transition-duration-150 ease-in-out hover:bg-white hover:text-black"
         data-test-id="cart-button"
         onClick={() => setIsModalOpen(true)}
       >
