e729c79a48a8e743b11ac06dbf0f21a1972b22f2
Alexis Guzman <alexis12989x@gmail.com>
2021-08-22 16:13:58 -0500

    feat: set id with products-home value

--- components/home/products/index.tsx
+++ components/home/products/index.tsx
@@ -4,7 +4,10 @@ import {Product} from "product/types";
 
 const Products = ({products}: {products: Product[]}): JSX.Element => {
   return (
-    <section className="px-4 mt-24 mx-auto max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+    <section
+      className="px-4 mt-24 mx-auto max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
+      id="products-home"
+    >
       {products.map((productItem) => (
         <ProductItem key={productItem.id} productItem={productItem} />
       ))}
