d99fb26761b9a3a6eee405544267b852d49290cd
Alexis Guzman <alexis12989x@gmail.com>
2021-08-23 17:11:29 -0500

    feat: change tag from section to main

--- components/home/products/index.tsx
+++ components/home/products/index.tsx
@@ -6,14 +6,14 @@ import {IProduct} from "@/product/types";
 
 const Products: FC<{products: IProduct[]}> = ({products}) => {
   return (
-    <section
+    <main
       className="px-4 mt-24 mx-auto max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
       id="products-home"
     >
       {products.map((productItem) => (
         <ProductItem key={productItem.id} productItem={productItem} />
       ))}
-    </section>
+    </main>
   );
 };
 
