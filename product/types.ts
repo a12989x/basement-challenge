2c9edc5415dad6640b70e26c02012f9a780bdd84
Alexis Guzman <alexis12989x@gmail.com>
2021-08-23 10:22:01 -0500

    feat: create i product cart interface

--- product/types.ts
+++ product/types.ts
@@ -1,19 +1,31 @@
-export interface Option {
+export type TID = string;
+
+export interface IOption {
   label: string;
   values: string[];
-}
-export interface Product {
-  id: string;
-  image: string;
-  price: number;
-  name: string;
-  description: string;
-  options: Option[];
-  qty?: number | undefined;
-  cart?: IOptionCart[];
 }
 
 export interface IOptionCart {
   label: string;
   value: string;
+}
+
+export interface IProduct {
+  id: TID;
+  image: string;
+  price: number;
+  name: string;
+  description: string;
+  options: IOption[];
+}
+
+export interface IProductCart extends IOption {
+  id: TID;
+  image: string;
+  price: number;
+  name: string;
+  description: string;
+  options: IOption[];
+  qty: number;
+  cart: IOptionCart[];
 }
