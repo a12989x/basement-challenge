4216a125a0c44ba1cfb3c7f3a1441b9bb305d8c3
Alexis Guzman <alexis12989x@gmail.com>
2021-08-24 14:44:42 -0500

    feat(cypress): remove unit tests

--- cypress/integration/pages/home/Product.spec.js
+++ cypress/integration/pages/home/Product.spec.js
@@ -1,31 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Product - Unit", () => {
-  beforeEach(() => {
-    cy.visit("/");
-    cy.get("section section section.product:first").as("productItem");
-  });
-
-  it("should display the name of the product", () => {
-    cy.get("@productItem").find("div p:first").should("contain.text", "Black t-shirt");
-  });
-
-  it("should display the price of the product", () => {
-    cy.get("@productItem").find("div p:last").should("contain.text", "$ 7.95");
-  });
-
-  it("should display the image of the product with alt and src attribute", () => {
-    cy.get("@productItem").find("button div img").should("have.attr", "alt");
-    cy.get("@productItem").find("button div img").should("have.attr", "src");
-  });
-
-  it("should show add to cart text while hover", () => {
-    cy.get("@productItem").find("button").trigger("mouseover");
-    cy.get("@productItem").find("button p").should("contain.text", "Add to cart");
-  });
-});
-
-describe("Product - Integration", () => {
+describe("Product", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.get("section section section.product:first").as("productItem");
