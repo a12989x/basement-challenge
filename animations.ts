9db7f575697f11d73779e5eae738d2aaa11a0a63
Alexis Guzman <alexis12989x@gmail.com>
2021-08-24 10:23:37 -0500

    feat(framer-motion): add create animations file

---
+++ animations.ts
@@ -0,0 +1,23 @@
+export const simpleYAndOpacity = {
+  hidden: {y: 50, opacity: 0},
+  show: {y: 0, opacity: 1},
+  exit: {y: 50, opacity: 0},
+};
+
+export const simpleXAndOpacity = {
+  hidden: {x: 50, opacity: 0},
+  show: {x: 0, opacity: 1},
+  exit: {x: 50, opacity: 0},
+};
+
+export const productItemAdd = {
+  hidden: {y: 0, opacity: 0},
+  show: {y: -66, opacity: 1},
+  exit: {y: 0, opacity: 0},
+};
+
+export const simpleOpacity = {
+  hidden: {opacity: 0},
+  show: {opacity: 1},
+  exit: {opacity: 0},
+};
